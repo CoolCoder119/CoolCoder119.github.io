adf6b6528d0c54157423c3436c7e61ec7877121f
CoolCoder119 <143909488+CoolCoder119@users.noreply.github.com>
2023-09-04 12:59:09 -0700

    readded js and made in case needed

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-console.log("javascript ran");
-var canvas = document.getElementById('Canvas');
-var ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-var width = canvas.width;
-var height = canvas.height;
-ctx.fillStyle = "black";
-ctx.fillRect(0,0,width,height)
+//in case code needs to be added
