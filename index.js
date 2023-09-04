4c826189edfd5807c79c7b50936b87f492402bbb
CoolCoder119 <143909488+CoolCoder119@users.noreply.github.com>
2023-09-04 12:39:26 -0700

    made first webpage look good!

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-var canvas =document.getElementById("canvas");
-canvas.backgroundColor = "rgba(10,10,256,0.3)"
+console.log("javascript ran");
+var canvas = document.getElementById('Canvas');
+var ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var width = canvas.width;
+var height = canvas.height;
+ctx.fillStyle = "black";
+ctx.fillRect(0,0,width,height)
