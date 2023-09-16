f2f091ae50884d1b1c5dcd79601e782d9efe6848
CoolCoder119 <143909488+CoolCoder119@users.noreply.github.com>
2023-09-15 17:32:45 -0700

    added more types and variety.

--- JAVASCRIPT/zombieShooter.js
+++ JAVASCRIPT/zombieShooter.js
@@ -1,3 +1,4 @@
+
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
@@ -5,39 +6,60 @@ canvas.height = window.innerHeight;
 var height = canvas.height;
 var width = canvas.width;
 
+var MULTIPLIER = width/1536;
 
 var projectiles = [];
 var enemies = [];
+var powerups = [];
 
 var playerColor = "red";
 var projectileColor = "pink";
 var enemyColor = "teal";
 
+var enemyInfo = function(health,radius,speed) {
+    this.health = health
+    this.radius = radius;
+    this.speed = speed;
+}
 
-var enemyRadius = 23;
-var enemySpeed = 0.5;
-var enemyMaxSpeed = 60;
-var maxEnemyHealth = 20;
-
+var basicEnemy = new enemyInfo(1,16*MULTIPLIER,1*MULTIPLIER);
+var mediumEnemy = new enemyInfo(5,20*MULTIPLIER,0.6*MULTIPLIER);
+var hardEnemy = new enemyInfo(30,40*MULTIPLIER,0.2);
+var ultraEnemy = new enemyInfo(100,60*MULTIPLIER,0.1);
 
 var gameFrame = 0;
 
 
-var playerRadius = 20;
-var playerSpeed = 5;
+var playerRadius = 20*MULTIPLIER;
+var playerSpeed = 3*MULTIPLIER;
+var hasPowerup = true;
+var powerupMaxLength = 300;
+var powerUpLength = powerupMaxLength;
 
 
-var projectileRadius = 10;
-var projectileSpeed = 14;
+var projectileRadius = 8*MULTIPLIER;
+var projectileSpeed = 10*MULTIPLIER;
+var projectileDamage = 1;
 
 var player;
 
 
-var enemySummonTick = 30;
-var bulletSummonTick = 3;
+var enemySummonTick = 50;
+var bulletSummonTick = 20;
+var powerupSummonTick = 1000;
+
+
+
+var powerupColor = "yellow";
+var powerupWidth = 100*MULTIPLIER;
+var powerupHeight = 100*MULTIPLIER;
 
 var score = 0;
 var gameOver = false;
+
+
+
+
 
 function gameover() {
     gameOver = true;
@@ -47,9 +69,9 @@ function gameover() {
     ctx.textBaseline = "middle";
     ctx.fillText("Game Over", width/2,height/2);    
 
-    resetGame();
-    //document.querySelector(".deadDiv").style.display = "inline-block";
-    //document.querySelector("#finalScore").innerHTML = "Score: " +score;
+
+    document.querySelector(".deadDiv").style.display = "inline-block";
+    document.querySelector("#finalScore").innerHTML = "Score: " +score;
 };
 function resetGame() {
 
@@ -58,6 +80,7 @@ function resetGame() {
     document.getElementById("score").innerHTML = "Score: 0";
     enemies = [];
     gameOver = false;
+    gameLoop();
 };
 
 document.querySelector(".restartButton").onclick = function() {
@@ -72,7 +95,13 @@ function getDistance(x1,y1,x2,y2) {
     return Math.sqrt(Math.pow(xDistance,2) + 
         Math.pow(yDistance,2));
 }
-function drawHealthBar(x,y,width,height,health,maxHealth,yOffset) {
+function touching(x1,x2,y1,y2,width1,width2,height1,height2) {
+    return x1+width1 > x2 &&
+           y1+height1 > y2 &&
+           x1 <= x2+width2 &&
+           y1 <= y2+height2;
+}
+function drawBar(x,y,width,height,health,maxHealth,yOffset) {
     var xPosition = x - width/2;
     var yPosition = y - yOffset;
     ctx.fillStyle = "red";
@@ -101,11 +130,21 @@ var Player = function(x,y,radius,color) {
     this.x = x;
     this.y = y;
     this.radius = radius;
+    this.xOffset = this.radius;
+    this.yOffset = this.radius;
+    this.width = radius*2;
+    this.height = radius*2;
     this.color = color;
     this.speed = playerSpeed;
 }
 Player.prototype.draw = function() {
     circle(this.x,this.y,this.radius,this.color);
+
+    var powerupBarX = width * 0.5;
+    var powerupBarY = width * 0.75;
+    var powerupBarWidth = width * 0.4;
+    var powerupBarHeight = height * 0.05;
+    drawBar(powerupBarX,powerupBarY,powerupBarWidth,powerupBarHeight,powerUpLength,powerupMaxLength);
 }
 Player.prototype.update = function() {
     if (Mouse.keyW && this.y - this.radius< 0 !== true) {
@@ -120,12 +159,22 @@ Player.prototype.update = function() {
     if (Mouse.keyD && this.x + this.radius > width !== true) {
         this.x += this.speed;
     }
-
     enemies.forEach((enemy) => {
         if (getDistance(this.x,this.y,enemy.x,enemy.y) < this.radius + enemy.radius) {
             gameover();
         }
       });
+      powerups.forEach((powerup) => {
+            var x = this.x-this.xOffset;
+            var y = this.y-this.yOffset;
+            var width = this.width;
+            var height = this.height;
+            var isTouching = touching(x,powerup.x,y,powerup.y,width,powerup.width,height,powerup.height);
+            if (isTouching) {
+                powerup.markedForDeletion = true;
+            }
+      });
+    
 };
 
 var Enemy = function(x,y,radius,color,xVel,yVel,speedMultiplier,health) {
@@ -141,7 +190,7 @@ var Enemy = function(x,y,radius,color,xVel,yVel,speedMultiplier,health) {
 }
 Enemy.prototype.draw = function() {
     circle(this.x,this.y,this.radius,this.color);
-    drawHealthBar(this.x,this.y,(this.radius * 2) / 0.75,10,this.health,this.maxHealth,this.radius + 15);
+    drawBar(this.x,this.y,(this.radius * 2) / 0.75,5,this.health,this.maxHealth,this.radius * 1.2);
 }
 Enemy.prototype.update = function() {
     var xDiff = player.x - this.x;
@@ -184,8 +233,7 @@ Projectile.prototype.update = function() {
     enemies.forEach((enemy) => {
         if (getDistance(this.x,this.y,enemy.x,enemy.y) < this.radius + enemy.radius) {
             this.markedForDeletion = true;
-            enemy.health -=1;
-            enemy.radius = enemy.health * 3 + 10;
+            enemy.health -=projectileDamage;
             if (enemy.health < 1) {
                 score += enemy.maxHealth;
                 document.getElementById("score").innerHTML = "Score: " + score;
@@ -196,16 +244,95 @@ Projectile.prototype.update = function() {
       });
 }
 
+var Powerup = function(x,y,width,height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = powerupColor;
+    this.markedForDeletion = false;
+}
+Powerup.prototype.draw = function() {
+    ctx.fillRect(this.x,this.y,this.width,this.height);
+}
+
+var summonNewProjectile = function() {
+    var x = player.x - player.radius;
+    var y = player.y - player.radius;
+    const angle = Math.atan2(
+    Mouse.y-y,
+    Mouse.x-x
+    );
+    const xVel = Math.cos(angle) * projectileSpeed;
+    const yVel = Math.sin(angle) * projectileSpeed;
+
+    var projectile = new Projectile(player.x, player.y,xVel,yVel,projectileRadius,projectileColor);
+    projectiles.push(projectile)
+}
+
+var summonNewEnemy = function() {
+    var x = Math.random() * width;
+    var y = Math.random() * height;
+    x = 0;
+    var xDiff = player.x - x;
+    var yDiff = player.y - y;
+    var angle = Math.atan2(yDiff,xDiff);
+    var xVel = Math.sin(angle) * enemySpeed;
+    var yVel = Math.cos(angle) * enemySpeed;
+    var random1 = Math.round(Math.random() * 365);
+    var random2 = Math.round(Math.random() * 365);
+    var random3 = Math.round(Math.random() * 365);
+    var random4 = Math.random();
+    var color = "rgb(" + random1 + ","+random2 + ","+random3 + ")";
+    var enemyHealth = Math.round(Math.random() * 100);
+    var Radius;
+    var enemySpeed;
+    var enemyHealth;
+    if (random4 < 0.5) {
+        var Radius = basicEnemy.radius;
+        var enemySpeed = basicEnemy.speed;
+        var enemyHealth = basicEnemy.health;
+    } else if (random4 < 0.8) {
+        var Radius = mediumEnemy.radius;
+        var enemySpeed = mediumEnemy.speed;
+        var enemyHealth = mediumEnemy.health;
+    } else if (random4 < 0.95) {
+        var Radius = hardEnemy.radius;
+        var enemySpeed = hardEnemy.speed;
+        var enemyHealth = hardEnemy.health;            
+    } else {
+        var Radius = ultraEnemy.radius;
+        var enemySpeed = ultraEnemy.speed;
+        var enemyHealth = ultraEnemy.health;             
+    }
+    var enemy = new Enemy(x,y,Radius,color,xVel,yVel,enemySpeed,enemyHealth);
+    enemies.push(enemy)
+}
+
+var summonNewPowerup = function() {
+    var thisPowerupWidth = powerupWidth;
+    var thisPowerupHeight = powerupHeight;
+    var x = Math.random() * width;
+    var y = Math.random() * height;
+    var powerup = new Powerup(x,y,thisPowerupWidth,thisPowerupHeight);
+    powerups.push(powerup);
+    powerupSummonTick = 1000;
+}
+
+
+
 
 
 resetGame();
+
+
 function gameLoop() {
     if (gameOver) {
         return;
     }
     ctx.fillStyle = "rgba(0,0,0,0.1)";
-    ctx.fillRect(0,0,width,height);
 
+    ctx.fillRect(0,0,width,height);
 
     player.update();
     player.draw();
@@ -222,45 +349,21 @@ function gameLoop() {
         enemy.draw();
         enemy.update();
       });
-    if (gameFrame % enemySummonTick === 0) {
-        var x = Math.random() * width;
-        var y = Math.random() * height;
-        if (Math.random() < 0.5) {
-            x = 0;
-        } else if (Math.random() < 0.5) {
-            y = 0;
-        } else if (Math.random() < 0.5) {
-            x = width;
-        } else {
-            y = height;
+      powerups.forEach((powerup,i) => {
+        powerup.draw();
+        if (powerup.markedForDeletion) {
+            powerups.splice(i,1);
+            console.log("splcied");
         }
-        var xDiff = player.x - x;
-        var yDiff = player.y - y;
-        var angle = Math.atan2(yDiff,xDiff);
-        var xVel = Math.sin(angle) * enemySpeed;
-        var yVel = Math.cos(angle) * enemySpeed;
-        var random1 = Math.round(Math.random() * 365);
-        var random2 = Math.round(Math.random() * 365);
-        var random3 = Math.round(Math.random() * 365);
-        var color = "rgb(" + random1 + ","+random2 + ","+random3 + ")";
-        var enemyHealth = Math.round(Math.random() * maxEnemyHealth);
-        var Radius = enemyHealth * 3 + 10;
-        var enemySpeed = enemyMaxSpeed/Radius;
-        var enemy = new Enemy(x,y,Radius,color,xVel,yVel,enemySpeed,enemyHealth,);
-        enemies.push(enemy)
+      });
+    if (gameFrame % enemySummonTick === 0) {
+        summonNewEnemy();
     }
     if (gameFrame % bulletSummonTick === 0) {
-        var x = player.x - player.radius;
-        var y = player.y - player.radius;
-        const angle = Math.atan2(
-        Mouse.y-y,
-        Mouse.x-x
-        );
-        const xVel = Math.cos(angle) * projectileSpeed;
-        const yVel = Math.sin(angle) * projectileSpeed;
-    
-        var projectile = new Projectile(player.x, player.y,xVel,yVel,projectileRadius,projectileColor);
-        projectiles.push(projectile)
+        summonNewProjectile();
+    }
+    if (gameFrame % powerupSummonTick === 0) {
+        summonNewPowerup();
     }
     gameFrame++;
     requestAnimationFrame(gameLoop);
