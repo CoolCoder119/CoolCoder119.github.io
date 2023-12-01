d471d5d1895ebe5ebbad90c74be043cf4352b7f1
CoolCoder119 <143909488+CoolCoder119@users.noreply.github.com>
2023-12-01 14:33:35 -0800

    Revert "Revert "made bad changes""
    This reverts commit 52ee8a4134cc7c2758852635aae8b92f4933e197.

--- ChessOnline/public/JS/variables.js
+++ ChessOnline/public/JS/variables.js
@@ -3,5 +3,44 @@ const ctx = canvas.getContext('2d');
 const socket = io();
 canvas.width = 400;
 canvas.height = 400;
+const canvasInfo = canvas.getBoundingClientRect();
 const width = canvas.width;
 const height = canvas.height;
+
+const board = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+];
+const columns = 8;
+const rows = 8;
+const blockSize = width/rows;
+
+var Black =  {
+        bishop : undefined,
+        king : undefined,
+        knight : undefined,
+        pawn : undefined,
+        queen : undefined,
+        rook : undefined
+    }
+var White = {
+    bishop : undefined,
+    king : undefined,
+    knight : undefined,
+    pawn : undefined,
+    queen : undefined,
+    rook : undefined
+}
+
+var Mouse = {
+    x: undefined,
+    y: undefined,
+    selected: false,
+    selectedPiece: undefined
+}
