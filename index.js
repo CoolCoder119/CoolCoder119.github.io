fc3f1d98017e55dc6e52c49456f96b31b014408c
SonicBuster3098 <140475727+SonicBuster3098@users.noreply.github.com>
2024-11-06 18:17:15 +0000

    js button control

---
+++ index.js
@@ -0,0 +1,11 @@
+const textInput = document.getElementsByClassName("Input")
+
+
+function sendToAPI(){
+    document.querySelector(".button").
+        onclick = function (){
+            alert("button clikced XD")
+        }
+}
+
+
